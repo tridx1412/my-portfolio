a0982d232d43245d757b771350f62bb348da8b50
tridx1412 <tridx1412@gmail.com>
2026-02-02 17:34:14 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Portfolio đã load thành công!");
+
+  // Hiệu ứng chào mừng nhẹ nhàng
+  setTimeout(() => {
+    alert("🎉 Chào mừng bạn đến với Portfolio cá nhân!");
+  }, 800);
+});
